9bc5c777b3843cd73f171b1de0b2c8f354fd4cd8
tranenn <aamandatran@gmail.com>
2024-04-18 23:00:57 +0200

    Reset comments

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ import { renderLogin } from "./registerLogin.js";
 import { renderGame } from "./main.js";
 
 
-// (function(){
-//     if(window.localStorage.getItem("user")){
-//         renderHomepage()
-//     } else {
-//         renderLogin()
-//     }
+(function () {
+    if (window.localStorage.getItem("user")) {
+        renderHomepage()
+    } else {
+        renderLogin()
+    }
 
 
-// window.addEventListener("load", () => {
-//     if (window.location.hash === "#game") {
-//         renderGame();
-//     } else {
-//         renderHomepage();
-//     }
-// });
-// })()
-
-renderLogin()
+    window.addEventListener("load", () => {
+        if (window.location.hash === "#game") {
+            renderGame();
+        } else {
+            renderHomepage();
+        }
+    });
+})()

2e9750a762675d51f5d783f6b28ac01c67a15237
tranenn <aamandatran@gmail.com>
2024-04-22 00:53:54 +0200

    Added CSS for characters

--- main.js
+++ main.js
@@ -52,34 +52,34 @@ function renderCharacters() {
 
     const characters = [
         {
-            name: "Freddan",
-            img: "src",
+            name: "FREDDAN",
+            img: "media/fredde.png",
             description: `Freddan 39 år och jobbar som VD och fondförvaltare på banken. Han bor i ett av de flottaste husen i det mycket fashionabla området Solsidan, Saltsjöbaden utanför Stockholm, där han även är uppväxt. Fredde är gift med Mikaela Schiller, som kallas Mickan, och tillsammans har de två barn, Victor och Ebba. Han är bästa vän med Alex Löfström, en barndomsvän som nyligen flyttat tillbaka till Solsidan med sin sambo Anna. Fredde är mycket entusiastisk i det som han engagerar sig i, men handlar oftast först och tänker sen. Han tänker mycket på status och lägger därpå mycket pengar och omsorg.`
         },
         {
-            name: "Mickan",
-            img: "src",
+            name: "MICKAN",
+            img: "media/mickan.png",
             description: `Mickan är gift med Fredde Schiller, Solsidans rikaste man. Hon är mammaledig med dottern Ebba men driver även ett företag som designar barnkläder. Mickan är precis som Fredde beroende av sin status i området, och vill vara "The first lady of Saltis", men har sin väninna Lussan att tävla mot. I säsong två återvänder en annan av Freddes barndomskamrater till Saltis, och visar sig vara tillsammans med Lussan, han visar sig även vara rikare än Fredde.`
         },
         {
-            name: "Alex",
-            img: "src",
-            description: `är en 39-årig tandläkare, med egen klinik. Han är uppvuxen i Saltsjöbaden utanför Stockholm. I första avsnittet flyttar Alex och sambon (senare gift) Anna Svensson till "Saltis" då han har köpt sitt barndomshem av sin mamma, Margareta Löfström. I den första säsongen väntar han och Anna barn som sedan föds i det sista avsnittet. Alex är konflikträdd och nojig, och bryr sig väldigt mycket om vad andra tycker om honom. Han bor granne med sin bästa vän, och barndomskamrat, Fredde Schiller och hans fru Mikaela.`
+            name: "ALEX",
+            img: "media/alex.png",
+            description: `Alex är en 39-årig tandläkare, med egen klinik. Han är uppvuxen i Saltsjöbaden utanför Stockholm. I första avsnittet flyttar Alex och sambon (senare gift) Anna Svensson till "Saltis" då han har köpt sitt barndomshem av sin mamma, Margareta Löfström. I den första säsongen väntar han och Anna barn som sedan föds i det sista avsnittet. Alex är konflikträdd och nojig, och bryr sig väldigt mycket om vad andra tycker om honom. Han bor granne med sin bästa vän, och barndomskamrat, Fredde Schiller och hans fru Mikaela.`
         },
         {
-            name: "Anette",
-            img: "src",
-            description: `är en fiktiv karaktär i humordrama-serien Solsidan.`
+            name: "ANETTE",
+            img: "media/anette.png",
+            description: `Anette är lika osympatisk som sin make Ove, och deras äktenskap kan bara beskrivas som en allians mellan två människor som delar samma tvivelaktiga karaktärsdrag.  Precis som Ove är Anette snål och saknar social kompetens, vilket gör henne till en svår person att umgås med för de flesta människor. Parets tillvaro tog en oväntad vändning när Anette fick tillgång till en stor summa pengar som ett resultat av en felaktig investering flera år tidigare. Trots deras nyfunna ekonomiska status, lyckades de inte förbättra sin sociala situation. Anette visar emellertid tecken på att hon tröttnar på sin make, men de lyckas alltid hitta tillbaka till varandra.`
         },
         {
-            name: "Ove",
-            img: "src",
-            description: `OVe är gammal skolbekant till Fredde och Alex. Ove är notoriskt snål och i total avsaknad av såväl social kompetens som självdistans, vilket resulterar i att han aldrig inser hur han uppfattas av omgivningen, att han beskrivs som en riktig skitstövel, eller att han aldrig är önskvärd eller välkommen. Ove vill ständigt låna prylar av grannarna, utan att lämna tillbaka dem eller själv låna ut något. Tidigare har han lyckats att - på oklart vis - bli ordförande i Saltsjöbadens Golfklubb. Han är gift med den lika osympatiska Annette med vilken han har dottern Marielle. Det var (lustigt nog) Oves och Anettes ide att Alex och Annas son skulle heta "Love". Ett snarlikt namn till "Ove". Trots Oves tämligen burdusa och buffliga stil följde de rådet.`
+            name: "OVE",
+            img: "media/ove.png",
+            description: `Ove är gammal skolbekant till Fredde och Alex. Ove är notoriskt snål och i total avsaknad av såväl social kompetens som självdistans, vilket resulterar i att han aldrig inser hur han uppfattas av omgivningen, att han beskrivs som en riktig skitstövel, eller att han aldrig är önskvärd eller välkommen. Ove vill ständigt låna prylar av grannarna, utan att lämna tillbaka dem eller själv låna ut något. Tidigare har han lyckats att - på oklart vis - bli ordförande i Saltsjöbadens Golfklubb. Han är gift med den lika osympatiska Annette med vilken han har dottern Marielle. Det var (lustigt nog) Oves och Anettes ide att Alex och Annas son skulle heta "Love". Ett snarlikt namn till "Ove". Trots Oves tämligen burdusa och buffliga stil följde de rådet.`
         },
         {
-            name: "Anna",
-            img: "src",
-            description: ` Anna är sambo (sedermera gift) med Alex Löfström och jobbar som skådespelerska. I första säsongen är hon ledig från sitt arbete eftersom hon är gravid och i säsong 2 så börjar hon jobba som massör. Anna är inte uppväxt i Saltsjöbaden till skillnad från Alex, och känner sig bitvis utanför och annorlunda än sin väninna Mickan och dennas väninnor. Hon strävar dock för att passa in, men är ibland lite väl verbal och rättfram.`
+            name: "ANNA",
+            img: "media/anna.png",
+            description: `Anna är sambo (sedermera gift) med Alex Löfström och jobbar som skådespelerska. I första säsongen är hon ledig från sitt arbete eftersom hon är gravid och i säsong 2 så börjar hon jobba som massör. Anna är inte uppväxt i Saltsjöbaden till skillnad från Alex, och känner sig bitvis utanför och annorlunda än sin väninna Mickan och dennas väninnor. Hon strävar dock för att passa in, men är ibland lite väl verbal och rättfram.`
         },
     ]
     let containerDom = main.querySelector("#containerCharacters");
@@ -89,8 +89,10 @@ function renderCharacters() {
         characterDom.classList.add("character");
 
         characterDom.innerHTML = `
-            <div class="topCharacter">                  
-                <img src="${character.img}" alt="${character.name}">
+            <div class="topCharacter">
+                <div class="imgBackground">
+                    <img src="${character.img}" alt="${character.name}">  
+                </div>                
                  <h3>${character.name}</h3>
             </div>
             <p>${character.description}</p>

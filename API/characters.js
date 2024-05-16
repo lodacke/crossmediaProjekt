04d259ee2b074157ea565078512637282c10ab81
tranenn <aamandatran@gmail.com>
2024-05-16 13:59:05 +0200

    Update characters.js

--- API/characters.js
+++ API/characters.js
@@ -3,40 +3,40 @@ export const characters = [
         name: "Fredde",
         fullName: "Fredrik Schiller",
         img: "media/gameIMG/Fredde.png",
-        description: `Fredrik Schiller är en affärsman som innehar positionen som VD och fondförvaltare i ett framgångsrikt men ospecificerat företag. Han bor tillsammans med sin fru Mikaela och deras tre barn i ett glamouröst område i Saltsjöbaden. Deras hem är ett av de mest imponerande och eftertraktade, vilket speglar familjens höga ställning och välstånd. Fredde är en väldigt entusiastisk och passionerad man. Han har dock en tendens att agera impulsivt, att handla först och tänka sen. Detta kan ibland leda till både framgångar och missöden, men det är en del av den personlighet som gör Fredde till den han är.`,
+        description: `Fredrik "Fredde" Schiller är en passionerad affärsman och VD i ett framgångsrikt företag. Han bor tillsammans med sin fru Mikaela och deras tre barn i ett glamouröst område i Saltsjöbaden. Deras hem är ett av de mest imponerande och eftertraktade, vilket speglar familjens höga ställning och välstånd. Fredde är en väldigt entusiastisk och passionerad man, särskilt när det kommer till projekt och intressen. Han har dock en tendens att agera impulsivt, att handla först och tänka sen. Detta kan ibland leda till både framgångar och missöden, men det är en del av den personlighet som gör Fredde till den han är. `,
         alibi: "<span>FEL</span>! Freddes dominanta natur förstärktes av sekten och den ytterligare rikedom han fick ta del av, men han var aldrig den som förvarade Anna."
     },
     {
         name: "Mickan",
         fullName: "Mikaela Schiller",
         img: "media/gameIMG/Mickan.png",
-        description: `Mikaela Schiller lever sitt liv i det välbärgade Saltsjöbaden tillsammans med sin make Fredrik. Mickans intressen inkluderar lyxiga shoppingrundor på Schuterman, promenader längs Saltsjöbadens stränder och att avnjuta ett glas vin tillsammans med hennes vänner. För Mickan är det viktigt att upprätthålla en fasad av framgång och perfektion. Men under ytan döljer sig en kvinna som kämpar med sin identitet och sina egna drömmar. Trots sin längtan efter självständighet och framgång är hon en hängiven familjekvinna som kämpar för att balansera sitt eget välbefinnande med sina ansvar gentemot sin familj och de hon älskar.`,
+        description: `Mikaela "Mickan" Schiller lever i det välbärgade Saltsjöbaden med sin make och tre barn. Trots sitt liv som hemmafru känner hon en längtan efter mer än att bara vara beroende av sin makes inkomster. Mickan kämpar med att hitta sin plats i ett samhälle där status och rykte är avgörande. Hon är intresserad av lyxig shopping, promenader och att umgås med sina ytliga vänner. Trots sin fasad av framgång kämpar Mickan med sin identitet och sina drömmar. Hon är bestämd och ordningsam hemma men längtar efter självständighet och framgång utanför hemmet.`,
         alibi: "<span>FEL</span>! Sekten litade inte på att Mickan skulle klara av att förvara Anna då hon tidigare i livet inte bevisat sitt intellekt. "
     },
     {
         name: "Alex",
         fullName: "Alexander Löfström",
         img: "media/gameIMG/Alex.png",
-        description: `Alexander Löfström är uppvuxen i Saltsjöbaden utanför Stockholm. Alex arbetar som tandläkare och har en egen klinik. Han har varit gift med Anna och de har tillsammans två barn. Alex är en karaktär som präglas av oro och konflikträdsla. Han bryr sig väldigt mycket om vad andra tycker om honom och varje beslut och handling vägs mot en osynlig mätare av andras åsikter. Trots detta har Alex ett gott hjärta och vill aldrig göra någon i sin omgivning besviken. Alexs barndomsvän och närmaste granne är Fredde Schiller och eras relation sträcker sig långt tillbaka till barndomen. Fredde fungerar som en stöttepelare i Alexs liv.`,
+        description: `Alex Löfström är en tandläkare som är uppvuxen och bor i Saltsjöbaden. Han äger sin barndoms hem som han har köpt av sin mamma Margareta. Han har varit gift med Anna och de har tillsammans två barn. Deras relation är fylld med både kärlek och konflikter och det är en konstant närvarande fråga om de ska hitta tillbaka till varandra. Alex är präglad av oro och rädsla för konflikter, han bryr sig mycket om andras åsikter, en börda som han bär med sig varje dag. Trots detta har Alex ett gott hjärta och vill aldrig göra någon i sin omgivning besviken. Fredde Schiller, hans barndomsvän och granne, ger honom trygghet och är en viktig del av hans liv.`,
         alibi: "<span>FEL</span>! Alex var bara en “vanlig” medlem i sekten och hans nervösa natur gjorde honom omförmögden att förvara Anna."
     },
     {
         name: "Anette",
         fullName: "Anette Sundberg",
         img: "media/gameIMG/Anette.png",
-        description: `Anette Sundberg är lika osympatisk som sin make Ove, och deras äktenskap kan bara beskrivas som en allians mellan två människor som delar samma tvivelaktiga karaktärsdrag.  Precis som Ove är Anette snål och saknar social kompetens, vilket gör henne till en svår person att umgås med för de flesta människor. Parets tillvaro tog en oväntad vändning när Anette fick tillgång till en stor summa pengar som ett resultat av en felaktig investering flera år tidigare. Trots deras nyfunna ekonomiska status, lyckades de inte förbättra sin sociala situation. Anette visar emellertid tecken på att hon tröttnar på sin make, men de lyckas alltid hitta tillbaka till varandra.`,
+        description: `Anette Sundberg är lika osympatisk som sin make Ove, och deras äktenskap kan bara beskrivas som en allians mellan två människor som delar samma tvivelaktiga karaktärsdrag. Precis som Ove är Anette snål och saknar social kompetens, vilket gör henne till en svår person att umgås med för de flesta människor. Parets tillvaro tog en oväntad vändning när Anette fick tillgång till en stor summa pengar som ett resultat av en felaktig investering flera år tidigare. Trots deras nyfunna ekonomiska status, lyckades de inte förbättra sin sociala situation. Anette visar emellertid tecken på att hon tröttnar på sin make, men de lyckas alltid hitta tillbaka till varandra. `,
         alibi: "<span>RÄTT</span>! Annette är sektens ledare och är den som har Anna i sitt förvar. Hon är alltid den som står upp och för talan!"
     },
     {
         name: "Ove",
         fullName: "Ove Sundberg",
         img: "media/gameIMG/Ove.png",
-        description: `Ove Sundberg är en person som är snål till den grad att det nästan blir en konstform, och hans totala brist på självdistans och social kompetens gör att han går igenom livet utan att förstå hur han uppfattas av andra. Trots hans uppenbara brister har han på något sätt lyckats bli ordförande i Saltsjöbadens Golfklubb. Ove är gift med Anette och tillsammans har de dottern Marielle. Trots att människorna i hans omgivning helst skulle vilja undvika honom till varje pris, dyker han alltid upp när de minst önskar det. Även om Ove är en uppenbart irriterande människa har han sina goda stunder.`,
+        description: `Ove Sundberg är en person som inte direkt sticker ut för sin charm eller sin förmåga att läsa sociala signaler. Han är snål till den grad att det nästan blir en konstform, och hans totala brist på självdistans och social kompetens gör att han går igenom livet utan att förstå hur han uppfattas av andra. Trots sina brister är han ordförande i Saltsjöbadens Golfklubb och gift med Anette, med dottern Marielle. Trots att han inte är omtyckt har han ändå en viss påverkan på sin omgivning och dyker alltid upp när man minst önskar det. Trots sin irriterande personlighet har Ove sina goda stunder och försöker vara trevlig, även om det ofta finns en baktanke med det han gör.`,
         alibi: "<span>FEL</span>! Övriga sektmedlemmar litade inte på att Ove var kapabel att förvara offret då han ansågs lättdistraherad. "
     },
     {
         name: "Anna",
         img: "media/gameIMG/Anna.png",
-        description: `Anna Svensson, en skådespelerska med en själ fylld av passion och drömmar, finner sig plötsligt boende i det pittoreska Saltsjöbaden då hon flyttade dit tillsammans med sin före detta make, Alex Löfström. För Anna är detta en ny värld, långt ifrån det rustika liv hon är van vid. Ibland känner hon sig som en främling i Saltsjöbaden. Hon är inte uppvuxen där och delar inte samma bakgrund eller referensramar som Alex och deras grannar. I strävan efter att passa in, finner hon sig själv kämpande för att hitta balansen mellan att vara verbal och rättfram, och samtidigt accepterad av de som omger henne.`,
+        description: `Anna Svensson, en skådespelerska med passion och drömmar, flyttar till Saltsjöbaden med sin före detta make, Alex Löfström, och deras två barn. Trots att hon inte har samma bakgrund som de lokala invånarna och ibland känner sig som en främling, tar hon sig an olika jobb och extraknäck med mod och ihärdighet. Anna strävar efter att passa in samtidigt som hon bevarar sin egen röst och personlighet. Anna är en kvinna med en stark vilja och stort hjärta. `,
     },
 ]
